48eb34a57388f582d9b8a9e6e1556dcf873fa4c9
Seth Hildebrand <smhilde@gmail.com>
2015-01-22 22:08:17 -0600

    Finished responsive CSS/HTML work.

--- dhhd/plan/urls.py
+++ dhhd/plan/urls.py
@@ -4,6 +4,6 @@ from plan import views
 urlpatterns = patterns('',
 	url(r'^$', views.index2, name='plan'),
 	url(r'^(?P<plan_number>[\d-]+)/$', views.details, name='details'),
-	url(r'^test/$', views.plan_index),
+	# url(r'^test/$', views.plan_index),
 	url(r'^like_plan/$', views.like_plan, name='like_plan'),
 	)
