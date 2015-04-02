b9dcce5ccf67f0778ece60cbbc18c4fa899998cd
Seth Hildebrand <smhilde@gmail.com>
2015-04-01 21:03:26 -0500

    Remove old jpgs, updates cart.html and dhhd-stripe.js

--- dhhd/static/scripts/dhhd-stripe.js
+++ dhhd/static/scripts/dhhd-stripe.js
@@ -1,6 +1,14 @@
 Stripe.setPublishableKey('pk_test_O7x17CwMK7CDj4tOzK0cSoJT');
 
-
+function reportError(msg) {
+    var $form = $('#payment-form');
+    // Show the error in the form:
+    $form.find('.payment-errors').text(msg).addClass('alert alert-error');
+    // re-enable the submit button
+    $form.find('button').prop('disabled', false);
+    return false;
+}
+    
 function stripeResponseHandler(status, response) {
   var $form = $('#payment-form');
 
@@ -19,16 +27,52 @@ function stripeResponseHandler(status, response) {
 };
 
 
-jQuery(function($) {
+// Watch for the document to be ready
+$(document).ready(function() {
+
+  // Watch for a form submission:
   $('#payment-form').submit(function(event) {
     var $form = $(this);
+    
+    // Flag variable
+    var error = false;
 
     // Disable the submit button to prevent repeated clicks
     $form.find('button').prop('disabled', true);
 
-    Stripe.card.createToken($form, stripeResponseHandler);
+    // Get the values
+    var ccNum = $('.card-number').val();
+    var cvcNum = $('.card-cvc').val();
+    var expMonth = $('.card-expiry-month').val();
+    var expYear = $('.card-expiry-year').val();
+    
+    // Validate the number
+    if (!Stripe.validateCardNumber(ccNum)) {
+      error = true;
+      reportError('The credit card number appears to be invalid.');
+    }
+    
+    // Validate the CVC number
+    if (!Stripe.validateCVC(cvcNum)) {
+      error = true;
+      reportError('The CVC number appears to be invalid.');
+    }
+    
+    // Vaildate the expiration
+    if (!Stripe.validateExpiry(expMonth, expYear)) {
+      error = true;
+      reportError('The expiration date appears to be invalid.')
+    }
+    
+    // Check for errors:
+    if (!error) {
+    
+      // Get the Stripe token
+      Stripe.card.createToken($form, stripeResponseHandler);
+    }
 
     // Prevent the form from submitting with the default action
     return false;
-  });
-});
+    
+  }); // Form submission
+}); // Document ready
