9bcce035ee59cd7c47b5f2d447ba551a2dce3d70
Seth Hildebrand <smhilde@gmail.com>
2015-04-11 14:35:47 -0500

    Adds Checkout page to store.

--- dhhd/store/views.py
+++ dhhd/store/views.py
@@ -1,4 +1,8 @@
+from __future__ import print_function
+
 import re
+import json
+import pprint
 
 from django.shortcuts import render, render_to_response, redirect
 from django.http import HttpResponse
@@ -55,6 +59,10 @@ def checkout(request):
 			description = ', '.join([request.POST['customer_name'], 'Plan ' + request.POST['plan_number']]),
 			statement_descriptor = 'Don Designs Plan ' + request.POST['plan_number'],
 			receipt_email = request.POST['receipt_email'],
+			metadata = {'email': request.POST['receipt_email'],
+						'plan_number': request.POST['plan_number'],
+						'shipping_method': 'Priority Ground',
+						},
 		)
 	except stripe.error.CardError as e:
 		# There was an error with the card
@@ -71,14 +79,75 @@ def checkout(request):
 		plan_list = Plan.objects.get(number=request.POST['plan_number'])
 		plan_list = plan.views.reformat_plan(plan_list)
 		context_dict['plan_object'] = plan_list
-		print('Status is: {}'.format(e.http_status))
-		print('Type is: {}'.format(err.get('type')))
-		print('Code is: {}'.format(err.get('code')))
-		print('Params is: {}'.format(err.get('param')))
-		print('Message is: {}'.format(err.get('message')))
+		#print('Status is: {}'.format(e.http_status))
+		#print('Type is: {}'.format(err.get('type')))
+		#print('Code is: {}'.format(err.get('code')))
+		#print('Params is: {}'.format(err.get('param')))
+		#print('Message is: {}'.format(err.get('message')))
 		return render(request, 'store/cart.html', context_dict)
-		
-	return render(request, 'store/checkout.html', {})
+	except stripe.error.InvalidRequestError as e:
+		# Invalid parameters were supplied to Stripe's API
+		body = e.json_body
+		err = body.get('error')
+		context_dict = {}
+		context_dict['err'] = err
+		form = CartForm(request.POST)
+		context_dict['form'] = form
+		plan_list = Plan.objects.get(number=request.POST['plan_number'])
+		plan_list = plan.views.reformat_plan(plan_list)
+		context_dict['plan_object'] = plan_list
+		return render(request, 'store/cart.html', context_dict)
+	except stripe.error.AuthenticationError as e:
+		# Authentication with Stripe's API failed. Are the keys correct?
+		body = e.json_body
+		err = body.get('error')
+		context_dict = {}
+		context_dict['err'] = err
+		form = CartForm(request.POST)
+		context_dict['form'] = form
+		plan_list = Plan.objects.get(number=request.POST['plan_number'])
+		plan_list = plan.views.reformat_plan(plan_list)
+		context_dict['plan_object'] = plan_list
+		return render(request, 'store/cart.html', context_dict)
+	except stripe.error.APIConnectionError as e:
+		# Network communication with Stripe failed
+		body = e.json_body
+		err = body.get('error')
+		context_dict = {}
+		context_dict['err'] = err
+		form = CartForm(request.POST)
+		context_dict['form'] = form
+		plan_list = Plan.objects.get(number=request.POST['plan_number'])
+		plan_list = plan.views.reformat_plan(plan_list)
+		context_dict['plan_object'] = plan_list
+		return render(request, 'store/cart.html', context_dict)
+	except stripe.error.StripeError as e:
+		# Display a very generic error to the user, and maybe send yourself and email
+		body = e.json_body
+		err = body.get('error')
+		context_dict = {}
+		context_dict['err'] = err
+		form = CartForm(request.POST)
+		context_dict['form'] = form
+		plan_list = Plan.objects.get(number=request.POST['plan_number'])
+		plan_list = plan.views.reformat_plan(plan_list)
+		context_dict['plan_object'] = plan_list
+		return render(request, 'store/cart.html', context_dict)
+	except Exception as e:
+		# This covers all other exceptions. It might not be great to keep this exception.
+		body = e.json_body
+		err = body.get('error')
+		context_dict = {}
+		context_dict['err'] = err
+		form = CartForm(request.POST)
+		context_dict['form'] = form
+		plan_list = Plan.objects.get(number=request.POST['plan_number'])
+		plan_list = plan.views.reformat_plan(plan_list)
+		context_dict['plan_object'] = plan_list
+		return render(request, 'store/cart.html', context_dict)
+	price = '${:.2f}'.format(float(charge.amount)/100)
+	plan_object = Plan.objects.get(number=charge['metadata']['plan_number'])
+	return render(request, 'store/checkout.html', {'charge': charge, 'plan_object': plan_object, 'price': price})
 
 def terms(request):
 	return render(request, 'store/terms.html', {})
